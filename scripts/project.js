67653ff48873d822ca1ab43de0ffe87f257ace85
David Antonio Fajardo Ponce <davidagape@hotmail.com>
2023-10-28 15:40:49 -0600

    Add files via upload
    Signed-off-by: David Antonio Fajardo Ponce <davidagape@hotmail.com>

--- scripts/project.js
+++ scripts/project.js
@@ -1,37 +1,46 @@
-let weight_kg = Number(document.querySelector("#weight").value);
-let height_cm = Number(document.querySelector("#height").value);
-let age_years = Number(document.querySelector("#age").value);
-
-function basal_metabolic_rate(weight_kg, height_cm, age_years, gender) {
+/*CSE 121 PROJECT DAILY CALORIES NEEDS*/
+/*AUTHOR: DAVID ANTONIO FAJARDO PONCE*/
+/*Functions for the project*/
+function basal_metabolic_rate() {
+  let weight = Number(document.getElementById("weight").value);
+  let height = Number(document.getElementById("height").value);
+  let age = Number(document.getElementById("age").value);
   let gender = document.querySelector("#gender").value;
   if (gender == "male") {
-    const bmr =
-      66.5 + 13.75 * weight_kg + 5.003 * height_cm - 6.755 * age_years;
+    bmr = 66.5 + 13.75 * weight + 5.003 * height - 6.755 * age;
     return bmr;
   } else {
-    const bmr =
-      655.1 + 9.563 * weight_kg + 1.85 * height_cm - 4.676 * age_years;
+    bmr = 655.1 + 9.563 * weight + 1.85 * height - 4.676 * age;
     return bmr;
   }
 }
 function dailyCaloriesNeeds() {
   let activity = document.querySelector("#activity").value;
+  let bmr = basal_metabolic_rate();
+  let element = document.querySelector("#daily_calories");
+
   if (activity == "sedentary") {
     const dailyCalories = bmr * 1.2;
+    element.value = dailyCalories;
     return dailyCalories;
   } else if (activity == "light") {
     const dailyCalories = bmr * 1.375;
+    element.value = dailyCalories;
     return dailyCalories;
   } else if (activity == "moderate") {
     const dailyCalories = bmr * 1.55;
+    element.value = dailyCalories;
     return dailyCalories;
   } else if (activity == "hard_exercise") {
     const dailyCalories = bmr * 1.725;
+    element.value = dailyCalories;
     return dailyCalories;
   } else {
     const dailyCalories = bmr * 1.9;
+    element.value = dailyCalories;
     return dailyCalories;
   }
 }
-const element = document.geyElementById("calories").innerHTML;
-element.addEventListener("click", dailyCaloriesNeeds);
+/* Event Listeners */
+const daily_calories_element = document.querySelector("#calories");
+daily_calories_element.addEventListener("click", dailyCaloriesNeeds);
